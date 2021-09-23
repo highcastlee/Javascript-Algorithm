e4e17f28b741d8c9a7c68ce14c03e0e5d46214b7
highcastlee <highcastlee@naver.com>
2021-09-24 01:08:46 +0900

    update 15988

--- 백준/15988.js
+++ 백준/15988.js
@@ -5,9 +5,14 @@ let input =
         .readFileSync('/dev/stdin')
         .toString()
         .split('\n')
-    : `3
-4
-7`.split('\n')
+    : `7
+1
+10
+100
+1000
+10000
+100000
+1000000`.split('\n')
 
 const T = parseInt(input[0])
 
@@ -16,20 +21,23 @@ const Nums = input.slice(1).map(Number)
 sol(T, Nums)
 
 function sol (t, nums) {
-  const MAX = 1000000
-  const m = 1000000009
-  const maxNum = Math.max(...nums)
-  const store = new Array(MAX)
+  let m = 1000000009
+  let maxNum = Math.max(...nums)
+  let store = new Array(maxNum).fill(0)
   store[1] = 1
   store[2] = 2
   store[3] = 4
-  const result = []
   for (let i = 4; i <= maxNum; i++) {
-    store[i] = store[i - 1] + store[i - 2] + store[i - 3]
+    store[i] = (store[i - 1] + store[i - 2] + store[i - 3]) % m
   }
-  for (let num of nums) {
-    result.push(store[num])
+  for (let j = 0; j < t; j++) {
+    console.log(store[nums[j]])
   }
-  console.log(result)
-  return result
 }
+
+// 점화식만 잘 구하면 풀 수 있음
+// s[i] = s[i-1]+s[i-2]+s[i-3]
+
+// 주어진 매개변수를 다 사용해서 풀어야함
+// nums 개수와 t가 다른 것도 테스트 케이스에서 체크함
+// t, nums
